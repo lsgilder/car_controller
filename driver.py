38c99f562910acd594f13e8cd352a7480ec7c096
lsgilder <saltywalrus99@gmail.com>
2020-03-11 23:07:34 -0600

    It turns now

--- driver.py
+++ driver.py
@@ -14,22 +14,20 @@ def back(event):
     cont.send("0")
 
 
+def left(event):
+    cont.send("3")
+
+
+def right(event):
+    cont.send("4")
+
+
 def stop(event):
     cont.send("!")
 
 
 def quit_app():
     exit()
-
-
-def do_stuff(event):
-    print(event)
-    print("down")
-
-
-def do_things(event):
-    print(str(event))
-    print("up")
 
 
 class Window(tk.Frame):
@@ -44,22 +42,27 @@ class Window(tk.Frame):
         self.master.title("GUI")
         self.pack(fill="both", expand=1)
         forward_button = tk.Button(self, text="forward")
-        forward_button.place(x=0, y=0)
+        forward_button.place(relx=0.5, rely=0.45, anchor="center")
         forward_button.bind('<ButtonPress-1>', forward)
         forward_button.bind('<ButtonRelease-1>', stop)
 
         reverse_button = tk.Button(self, text="reverse")
-        reverse_button.place(x=0, y=60)
+        reverse_button.place(relx=0.5, rely=0.55, anchor="center")
         reverse_button.bind('<ButtonPress-1>', back)
         reverse_button.bind('<ButtonRelease-1>', stop)
 
-        quit_button = tk.Button(self, text="quit", command=quit_app)
-        quit_button.place(x=250, y=400)
+        left_button = tk.Button(self, text="left")
+        left_button.place(relx=0.4, rely=0.5, anchor="center")
+        left_button.bind('<ButtonPress-1>', left)
+        left_button.bind('<ButtonRelease-1>', stop)
 
-        test_button = tk.Button(self, text="test")
-        test_button.pack(side="right")
-        test_button.bind('<ButtonPress-1>', do_stuff)
-        test_button.bind('<ButtonRelease-1>', do_things)
+        right_button = tk.Button(self, text="right")
+        right_button.place(relx=0.6, rely=0.5, anchor="center")
+        right_button.bind('<ButtonPress-1>', right)
+        right_button.bind('<ButtonRelease-1>', stop)
+
+        quit_button = tk.Button(self, text="quit", command=quit_app)
+        quit_button.place(relx=0.5, rely=0.9, anchor="center")
 
 
 def main():
